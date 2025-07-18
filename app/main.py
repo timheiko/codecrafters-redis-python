10fe31fa1491b0ea4671e0c4a6255991d02843b3
Tim Heiko <175552092+timheiko@users.noreply.github.com>
2025-07-18 18:55:08 +0200

    Respond to PING #rg2
    In one terminal start the program
    ```
    % ./your_program.sh
    ```
    
    Send a message to Redis port 6379
    ```
    % redis-cli PING
    ```

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 import socket  # noqa: F401
+import sys
+
+
+def log(*args: list[any]) -> None:
+    print(*args, file=sys.stderr)
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+
+    while True:
+        connection, address = server_socket.accept()
+        log("address ", address)
+        
+        connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":

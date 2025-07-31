95ea6ce9d0a2eaf45afdcdf28ececb26eb09871b
Tim Heiko <175552092+timheiko@users.noreply.github.com>
2025-07-31 22:13:13 +0200

    REPS: Migrated if/elif/else to match/case

--- app/resp.py
+++ app/resp.py
@@ -8,31 +8,32 @@ def encode(data: any) -> bytes:
     Encode data into RESP format to send it to client
     https://redis-doc-test.readthedocs.io/en/latest/topics/protocol/
     """
-    if isinstance(data, str):
-        return (
-            b"$"
-            + str(len(data)).encode()
-            + LINE_SEPARATOR
-            + data.encode()
-            + LINE_SEPARATOR
-        )
-    elif isinstance(data, int):
-        return b":" + str(data).encode() + LINE_SEPARATOR
-    elif data is None:
-        return b"$-1" + LINE_SEPARATOR
-    elif isinstance(data, list):
-        return (
-            b"*"
-            + str(len(data)).encode()
-            + LINE_SEPARATOR
-            + b"".join(encode(item) for item in data)
-        )
-    elif isinstance(data, ValueError):
-        return f"-ERR {" ".join(data.args)}".encode() + LINE_SEPARATOR
-    elif isinstance(data, bytes):
-        return f"${len(data)}".encode() + LINE_SEPARATOR + data
-    else:
-        raise Exception(f"Unsupported encoding data type: {type(data)}: {data}")
+    match data:
+        case str(_):
+            return (
+                b"$"
+                + str(len(data)).encode()
+                + LINE_SEPARATOR
+                + data.encode()
+                + LINE_SEPARATOR
+            )
+        case int(_):
+            return b":" + str(data).encode() + LINE_SEPARATOR
+        case None:
+            return b"$-1" + LINE_SEPARATOR
+        case [*_]:
+            return (
+                b"*"
+                + str(len(data)).encode()
+                + LINE_SEPARATOR
+                + b"".join(encode(item) for item in data)
+            )
+        case ValueError():
+            return f"-ERR {" ".join(data.args)}".encode() + LINE_SEPARATOR
+        case bytes(_):
+            return f"${len(data)}".encode() + LINE_SEPARATOR + data
+        case _:
+            raise Exception(f"Unsupported encoding data type: {type(data)}: {data}")
 
 
 def encode_simple(data: str) -> bytes:
@@ -40,10 +41,11 @@ def encode_simple(data: str) -> bytes:
     Encode a string into RESP simple string format to send it to client
     https://redis-doc-test.readthedocs.io/en/latest/topics/protocol/
     """
-    if isinstance(data, str):
-        return b"+" + data.encode() + LINE_SEPARATOR
-    else:
-        raise Exception(f"Unsupported encoding data type: {type(data)}: {data}")
+    match data:
+        case str(_):
+            return b"+" + data.encode() + LINE_SEPARATOR
+        case _:
+            raise Exception(f"Unsupported encoding data type: {type(data)}: {data}")
 
 
 def decode(payload: bytes):
